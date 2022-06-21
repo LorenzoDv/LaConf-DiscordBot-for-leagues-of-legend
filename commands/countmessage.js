8854a36831574c500cec26abda2f118eaf1b3e96
LorenzoDv <lorenzo.devichi@hotmail.fr>
2022-06-21 19:56:14 +0200

    ADD OPTION

--- commands/countmessage.js
+++ commands/countmessage.js
@@ -7,25 +7,26 @@ const client = new discord.Client();
 //create a commande ton send a message 'leagues of legend est en cours de désinstallation' in an embed
 module.exports = {
     name: 'l',
-    execute(message, args, Discord) {
+    async execute(message, args, Discord) {
         let channel = message.client.channels.cache.get('792465530657308682');
         let role = message.guild.roles.cache.find(r => r.id === "689421225994354691");
         var user = message.author.id
-        channel.send(`<@${user}> t'invite sur League of legends - ${role}`);
-        //effacer le message avec un certain temps
+
+        // await channel.send(`<@${user}> t'invite sur League of legends - ${role}`)
+        channel.send(`<@${user}> t'invite sur League of legends - ${role}`).then(msg => {
+            msg.react('✅')
+            msg.react('❌')
+        }
+        )
+
+
+
         setTimeout(function () {
             message.delete();
         }
             , 50);
-
-
-
-
-    },
-};
-
-
-
+    }
+}
 
 
 
